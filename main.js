ac754fc277aec94a887ac2a8ec31b35b443802bb
Juanpablo Barahona <juanpablo.barahonaa@gmail.com>
2017-06-13 17:45:35 -0400

    Module menuBar

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const app = electron.app
 
 const path = require('path')
 const url = require('url')
+const menuBar = require('./src/app/menu')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -12,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1080, height: 760})
+  mainWindow = new BrowserWindow({width: 1080, height: 820})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -33,165 +34,9 @@ function createWindow () {
   })
 }
 
-let template = [{
-  label: 'Edit',
-  submenu: [{
-    label: 'Undo',
-    accelerator: 'CmdOrCtrl+Z',
-    role: 'undo'
-  }, {
-    label: 'Redo',
-    accelerator: 'Shift+CmdOrCtrl+Z',
-    role: 'redo'
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Cut',
-    accelerator: 'CmdOrCtrl+X',
-    role: 'cut'
-  }, {
-    label: 'Copy',
-    accelerator: 'CmdOrCtrl+C',
-    role: 'copy'
-  }, {
-    label: 'Paste',
-    accelerator: 'CmdOrCtrl+V',
-    role: 'paste'
-  }, {
-    label: 'Select All',
-    accelerator: 'CmdOrCtrl+A',
-    role: 'selectall'
-  }]
-}, {
-  label: 'View',
-  submenu: [{
-    label: 'Toggle Full Screen',
-    accelerator: (function () {
-      if (process.platform === 'darwin') {
-        return 'Ctrl+Command+F'
-      } else {
-        return 'F11'
-      }
-    })(),
-    click: function (item, focusedWindow) {
-      if (focusedWindow) {
-        focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
-      }
-    }
-  }, {
-    label: 'Toggle Developer Tools',
-    accelerator: (function () {
-      if (process.platform === 'darwin') {
-        return 'Alt+Command+I'
-      } else {
-        return 'Ctrl+Shift+I'
-      }
-    })(),
-    click: function (item, focusedWindow) {
-      if (focusedWindow) {
-        focusedWindow.toggleDevTools()
-      }
-    }
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Corsi Block Tapping Test',
-    click: function (item, focusedWindow) {
-      if (focusedWindow) {
-        const options = {
-          type: 'info',
-          title: 'Application Menu Demo',
-          buttons: ['Ok'],
-          message: 'Esta aplicación es desarrollada con fines de realizar pruebas de memoria a niños, analizar sus resultados y ayudar a mejorar sus transtornos.'
-        }
-        electron.dialog.showMessageBox(focusedWindow, options, function () {})
-      }
-    }
-  }]
-}, {
-  label: 'Window',
-  role: 'window',
-  submenu: [{
-    label: 'Minimize',
-    accelerator: 'CmdOrCtrl+M',
-    role: 'minimize'
-  }, {
-    label: 'Close',
-    accelerator: 'CmdOrCtrl+W',
-    role: 'close'
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Reopen Window',
-    accelerator: 'CmdOrCtrl+Shift+T',
-    enabled: false,
-    key: 'reopenMenuItem',
-    click: function () {
-      app.emit('activate')
-    }
-  }]
-}, {
-  label: 'Help',
-  role: 'help',
-  submenu: [{
-    label: 'Learn More',
-    click: function () {
-      electron.shell.openExternal('http://electron.atom.io')
-    }
-  }]
-}]
-
-function addUpdateMenuItems (items, position) {
-  if (process.mas) return
-
-  const version = electron.app.getVersion()
-  let updateItems = [{
-    label: `Version ${version}`,
-    enabled: false
-  }, {
-    label: 'Checking for Update',
-    enabled: false,
-    key: 'checkingForUpdate'
-  }, {
-    label: 'Check for Update',
-    visible: false,
-    key: 'checkForUpdate',
-    click: function () {
-      require('electron').autoUpdater.checkForUpdates()
-    }
-  }, {
-    label: 'Restart and Install Update',
-    enabled: true,
-    visible: false,
-    key: 'restartToUpdate',
-    click: function () {
-      require('electron').autoUpdater.quitAndInstall()
-    }
-  }]
-
-  items.splice.apply(items, [position, 0].concat(updateItems))
-}
-
-function findReopenMenuItem () {
-  const menu = Menu.getApplicationMenu()
-  if (!menu) return
-
-  let reopenMenuItem
-  menu.items.forEach(function (item) {
-    if (item.submenu) {
-      item.submenu.items.forEach(function (item) {
-        if (item.key === 'reopenMenuItem') {
-          reopenMenuItem = item
-        }
-      })
-    }
-  })
-  return reopenMenuItem
-}
-
 if (process.platform === 'darwin') {
   const name = electron.app.getName()
-  template.unshift({
+  menuBar.template.unshift({
     label: name,
     submenu: [{
       label: `About ${name}`,
@@ -227,33 +72,33 @@ if (process.platform === 'darwin') {
   })
 
   // Window menu.
-  template[3].submenu.push({
+  menuBar.template[3].submenu.push({
     type: 'separator'
   }, {
     label: 'Bring All to Front',
     role: 'front'
   })
 
-  addUpdateMenuItems(template[0].submenu, 1)
+  menuBar.addUpdateMenuItems(menuBar.template[0].submenu, 1)
 }
 
 if (process.platform === 'win32') {
-  const helpMenu = template[template.length - 1].submenu
-  addUpdateMenuItems(helpMenu, 0)
+  const helpMenu = menu.template[menu.template.length - 1].submenu
+  menuBar.addUpdateMenuItems(helpMenu, 0)
 }
 
 app.on('ready', function () {
-  const menu = Menu.buildFromTemplate(template)
+  const menu = Menu.buildFromTemplate(menuBar.template)
   Menu.setApplicationMenu(menu)
   createWindow()
 })
 
 app.on('browser-window-created', function () {
-  let reopenMenuItem = findReopenMenuItem()
+  let reopenMenuItem = menuBar.findReopenMenuItem()
   if (reopenMenuItem) reopenMenuItem.enabled = false
 })
 
 app.on('window-all-closed', function () {
-  let reopenMenuItem = findReopenMenuItem()
+  let reopenMenuItem = menuBar.findReopenMenuItem()
   if (reopenMenuItem) reopenMenuItem.enabled = true
 })
